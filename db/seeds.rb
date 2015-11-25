1424260b89519ca25078256d267f85019d077acf
Madelene Campos <madelenecampos@gmail.com>
2015-11-24 20:00:28 -0500

    Implemented strptime to make the start and end times readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+class DateConverter
+  def self.convert(value)
+    Date.strptime( value, '%m/%e/%y %k:%M') # parses custom date format into Date instance
+  end
+end
 
 filename = 'db/appt_data.csv'
-options = {:key_mapping => {:unwanted_row => nil, :old_row_name => :new_name}}
+options = {
+  :key_mapping => {:unwanted_row => nil, :old_row_name => :new_name},
+  :value_converters => {:start_time => DateConverter, :end_time => DateConverter}
+}
+
 n = SmarterCSV.process(filename, options) do |array|
       # we're passing a block in, to process each resulting hash / =row (the block takes array of hashes)
       # when chunking is not enabled, there is only one hash in each array
