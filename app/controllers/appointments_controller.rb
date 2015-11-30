ba6df3ac93694be43c279b3fc5bbe40dac8fa176
Madelene Campos <madelenecampos@gmail.com>
2015-11-30 11:30:53 -0500

    Fixed spacing in appointments controller and creating appts integration test

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,52 +5,53 @@ class AppointmentsController < ApplicationController
   def index
       appointments = Appointment.all #issues query to database
 
-      if start_time = params[:start_time]
+    if start_time = params[:start_time]
       appointments = appointments.where(start_time: start_time) #adding filters, dynamically, with "where" method
-      end
+    end
 
-      if end_time = params[:end_time]
+    if end_time = params[:end_time]
       appointments = appointments.where(end_time: end_time)
-      end
+    end
       render json: appointments, status: 200 #fetching all of the appointments and returning them as json
   end
 
 # POST
-    def create
-      appointment = Appointment.new(appointment_params)
+  def create
+  appointment = Appointment.new(appointment_params)
 
-      if appointment.valid
-        if appointment.save
-        render nothing: :true, status: 204, location: appointment #204 == no content
-        else
-        render json: appointment.errors, status: :unprocessable_entity #same as 422
-        end
+    if appointment.valid
+      if appointment.save
+          render nothing: :true, status: 204, location: appointment #204 == no content
       else
-      render json: appointment.errors, status: 422
+         render json: appointment.errors, status: :unprocessable_entity #same as 422
       end
+      else
+       render json: appointment.errors, status: 422
     end
+  end
 
 # PUT/PATCH
-    def update
-      if @appointment.update(appointment_params)
-        render json: @appointment, status: 200
-      else
-        render json: @appointment.errors, status: 422
-      end
+  def update
+    if @appointment.update(appointment_params)
+    render json: @appointment, status: 200
+    else
+    render json: @appointment.errors, status: 422
     end
+  end
 
 # DELETE
-    def destroy
+  def destroy
       @appointment.destroy #removes the record from the database
       head 204
-    end
+  end
 
     private
-    def appointment_params
-      params.require(:appointment).permit(:first_name, :last_name, :start_time, :end_time, :comments,)
-    end
 
-    def set_appt
+  def appointment_params
+      params.require(:appointment).permit(:first_name, :last_name, :start_time, :end_time, :comments,)
+  end
+
+  def set_appt
       @appointment = Appointment.find(params[:id])
-    end
+  end
 end
--- test/integration/creating_appts_test.rb
+++ test/integration/creating_appts_test.rb
@@ -4,48 +4,57 @@ class CreatingApptsTest < ActionDispatch::IntegrationTest
   test 'creates appointment' do
     post '/appointments',
       { appointment:
-          { first_name: 'Maddie', last_name: 'Campos', start_time: '12-09-2015T10:00:00.000Z', end_time: '12-9-2015T11:00:00.000Z' }
+          { first_name: 'Maddie', last_name: 'Campos', start_time: '2015-12-09T10:00:00.000Z', end_time: '2015-12-09T11:00:00.000Z' }
           }.to_json,
       { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s } #parse content in JSON
-      assert_equal 201, response.status #created
-      assert_equal Mime::JSON, response.content_type #content type of response is JSON
-    end
+      assert_equal 204, response.status
+  end
 
   test 'does not create appointment with first name nil' do
     post '/appointments',
     { appointment:
-      { first_name: nil, last_name: 'Campos', start_time: '12-09-2015T10:00:00.000Z', end_time: '12-09-2015T11:00:00.000Z' }
+      { first_name: nil, last_name: 'Campos', start_time: '2015-12-09T10:00:00.000Z', end_time: '2015-12-09T11:00:00.000Z' }
     }.to_json,
     { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     assert_equal 422, response.status # 422 == error occurred and client is to blame
-    end
+  end
 
   test 'does not create appointment with last name nil' do
     post '/appointments',
     { appointment:
-      { first_name: 'Maddie', last_name: nil, start_time: '12-09-2015T10:00:00.000Z', end_time: '12-09-2015T11:00:00.000Z' }
+      { first_name: 'Maddie', last_name: nil, start_time: '2015-12-09T10:00:00.000Z', end_time: '2015-12-09T11:00:00.000Z' }
     }.to_json,
     { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     assert_equal 422, response.status
-    end
+  end
 
 # When creating an appt, it is only valid if the start and end times are in the future
   test 'no start dates in the past' do
     post '/appointments',
     { appointment:
-      { first_name: 'Maddie', last_name: 'Campos', start_time: '11-23-2015T18:00:00.000Z', end_time: '11-23-2015T19:00:00.000Z' }
+      { first_name: 'Maddie', last_name: 'Campos', start_time: '2013-11-23T18:00:00.000Z', end_time: '2018-11-23T19:00:00.000Z' }
     }.to_json,
     { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     assert_equal 422, response.status #Unprocessable entity (well-formed request from client, but semantically invalid)
-    end
+  end
 
   test 'no end dates in the past' do
     post '/appointments',
     { appointment:
-      { first_name: 'Maddie', last_name: 'Campos', start_time: '11-23-2015T18:00:00.000Z', end_time: '11-23-2015T19:00:00.000Z' }
+      { first_name: 'Maddie', last_name: 'Campos', start_time: '2015-11-23T18:00:00.000Z', end_time: '2015-11-23T19:00:00.000Z' }
     }.to_json,
     { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     assert_equal 422, response.status
-    end
-end
+  end
 
+    test 'appointment time must not overlap other appointments' do
+      post '/appointments',
+  { appointment: { start_time: '2013-11-29T14:00:00.000Z',
+                    end_time: '2016-11-29T14:05:00:000Z',
+                    first_name: 'Maddie',
+                    last_name: 'Campos'}
+    }.to_json,
+    {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s}
+    assert_equal 422, response.status
+  end
+end
